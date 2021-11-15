7600d9beb9481433a9bd94f82b2fdab29517fb0a
Dan Wolner <daniel.wolner@gmail.com>
2021-11-15 11:08:39 -0800

    local env var

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ const withImages = require('next-images')
 module.exports = {
     ...withImages(),
     env: {
-        GMAPS_API_KEY: 'AIzaSyBe1MCdquXljVHyTzXwD194J8Eq6ubdaTs',
+        GMAPS_API_KEY: process.env.GOOGLE_API_KEY,
     },
 }
